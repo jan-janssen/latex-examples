76a4f1c79c6e3b23f9e15388ebfc0fa9fbdcc672
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-04-01 16:26:51 +0200

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,2 +1,3 @@
-c.LatexConfig.latex_command = '/tmp/texlive/bin/x86_64-linux/pdflatex -shell-esc'
+# c.LatexConfig.latex_command = '/tmp/texlive/bin/x86_64-linux/pdflatex -shell-esc'
+c.LatexConfig.latex_command = '/tmp/texlive/bin/x86_64-linux/pdflatex'
 c.LatexConfig.bib_command = '/tmp/texlive/bin/x86_64-linux/bibtex'
