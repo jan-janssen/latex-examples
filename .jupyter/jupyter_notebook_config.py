079c2f6bd21937ffe83e9a24a6b3bd2080a332cc
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-04-01 17:07:05 +0200

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,3 +1,3 @@
-c.LatexConfig.shell_escape == 'allow'
+c.LatexConfig.shell_escape = 'allow'
 c.LatexConfig.latex_command = '/tmp/texlive/bin/x86_64-linux/pdflatex'
 c.LatexConfig.bib_command = '/tmp/texlive/bin/x86_64-linux/bibtex'
