fcfd85674f9a7fd7580904ae721e3df9bd0fb658
Jan Janßen <janssen@mpie.de>
2020-04-01 10:34:31 +0200

    switch to conda

--- texconda_install.sh
+++ texconda_install.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env sh
+# Based on https://tex.stackexchange.com/questions/398830/how-to-build-my-latex-automatically-using-travis-ci
+
+# Originally from https://github.com/latex3/latex3
+
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
